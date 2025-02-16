62a4449eb2c57c7ecbe8b830fe743b01663436eb
David Rose <davdrose@stanford.edu>
2025-02-16 12:50:09 -0800

    updated plots

---
+++ code/experiments/experiment2/adults/js/trial.js
@@ -0,0 +1,76 @@
+
+
+const trial_obj = {
+    "version_1": {
+        "egg_ball": {
+            "choices": ["Andy", "Emily"],
+            "video": "videos/andy_correct_right_emily.mp4",
+        },
+        "phone_duck": {
+            "choices": ["Taylor", "Zoey"],
+            "video": "videos/zoey_correct_left_taylor.mp4",
+        },
+        "icecream_sunscreen": {
+            "choices": ["Olivia", "Mason"],
+            "video": "videos/olivia_correct_left_mason.mp4",
+        },
+        "vase_bear": {
+            "choices": ["Leo", "Sara"],
+            "video": "videos/leo_correct_right_sara.mp4",
+        },
+    },
+    "version_2": {
+        "egg_ball": {
+            "choices": ["Andy", "Emily"],
+            "video": "videos/emily_correct_right_andy.mp4",
+        },
+        "phone_duck": {
+            "choices": ["Taylor", "Zoey"],
+            "video": "videos/taylor_correct_left_zoey.mp4",
+        },
+        "icecream_sunscreen": {
+            "choices": ["Olivia", "Mason"],
+            "video": "videos/mason_correct_left_olivia.mp4",
+        },
+        "vase_bear": {
+            "choices": ["Leo", "Sara"],
+            "video": "videos/sara_correct_right_leo.mp4",
+        },
+    },
+    // "version_3": {
+    //     "egg_ball": {
+    //         "choices": ["Andy", "Emily"],
+    //         "video": "videos/andy_correct_left_emily.mp4",
+    //     },
+    //     "phone_duck": {
+    //         "choices": ["Taylor", "Zoey"],
+    //         "video": "videos/zoey_correct_right_taylor.mp4",
+    //     },
+    //     "icecream_sunscreen": {
+    //         "choices": ["Olivia", "Mason"],
+    //         "video": "videos/olivia_correct_right_mason.mp4",
+    //     },
+    //     "vase_bear": {
+    //         "choices": ["Leo", "Sara"],
+    //         "video": "videos/leo_correct_left_sara.mp4",
+    //     },
+    // },
+    // "version_3": {
+    //     "egg_ball": {
+    //         "choices": ["Andy", "Emily"],
+    //         "video": "videos/emily_correct_left_andy.mp4",
+    //     },
+    //     "phone_duck": {
+    //         "choices": ["Taylor", "Zoey"],
+    //         "video": "videos/taylor_correct_right_zoey.mp4",
+    //     },
+    //     "icecream_sunscreen": {
+    //         "choices": ["Olivia", "Mason"],
+    //         "video": "videos/mason_correct_right_olivia.mp4",
+    //     },
+    //     "vase_bear": {
+    //         "choices": ["Leo", "Sara"],
+    //         "video": "videos/sara_correct_left_leo.mp4",
+    //     },
+    // },    
+}
